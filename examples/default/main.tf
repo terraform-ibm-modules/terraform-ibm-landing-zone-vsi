61d3f99cfb01fdfb25548b7a000c26037eb4a4f6
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2022-08-17 09:16:50 +0100

    chore(deps): update terraform github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc to v2.2.0 (#60)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -48,7 +48,7 @@ data "ibm_is_ssh_key" "existing_ssh_key" {
 #############################################################################
 
 module "slz_vpc" {
-  source            = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc.git?ref=v2.1.0"
+  source            = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc.git?ref=v2.2.0"
   resource_group_id = local.resource_group_id
   region            = var.region
   prefix            = var.prefix
