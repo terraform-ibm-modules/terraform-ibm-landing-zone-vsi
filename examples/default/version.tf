ccfb83c47e94a91f23625535598575f7ee8fc326
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2022-12-01 18:20:56 +0000

    fix(deps): update required_provider (#175)
    * fix(deps): update required_provider
    
    * docs: committing files modified by the hook
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- examples/default/version.tf
+++ examples/default/version.tf
@@ -5,12 +5,12 @@ terraform {
     # Pin to the lowest provider version of the range defined in the main module's version.tf to ensure lowest version still works
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.45.0"
+      version = "1.48.0"
     }
     # The tls provider is not actually required by the module itself, just this example, so OK to use ">=" here instead of locking into a version
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 4.0.2"
+      version = ">= 4.0.4"
     }
   }
 }
