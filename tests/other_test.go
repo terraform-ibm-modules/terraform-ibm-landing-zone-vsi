645a242058fb2b42f41f8132ec36e7fa8d160538
Rajat Agrawal <rajatatibm@gmail.com>
2023-09-25 15:47:19 +0100

    feat: additional load balancer config
    SKIP UPGRADE TEST

--- tests/other_test.go
+++ tests/other_test.go
@@ -4,8 +4,6 @@ package test
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
-
-	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 )
 
 func TestRunBasicExample(t *testing.T) {
