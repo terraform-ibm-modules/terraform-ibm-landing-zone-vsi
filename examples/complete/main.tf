413d03be67eb1ad9672e73dbcea3ab419b4b67e1
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-11-10 23:16:22 +0000

    chore(deps): update terraform-module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -24,7 +24,7 @@ module "resource_group" {
 
 module "key_protect_all_inclusive" {
   source                    = "terraform-ibm-modules/key-protect-all-inclusive/ibm"
-  version                   = "4.4.0"
+  version                   = "4.4.1"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
   key_protect_instance_name = "${var.prefix}-kp"
@@ -60,7 +60,7 @@ data "ibm_is_ssh_key" "existing_ssh_key" {
 
 module "slz_vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.7.0"
+  version           = "7.10.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
