323a47d7fb313c606d0aa65b6a54646e1f01319e
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2023-12-18 18:22:42 +0000

    chore(deps): update terraform-module (#587)
    * chore(deps): update terraform-module
    
    * SKIP UPGRADE TEST
    
    ---------
    
    Co-authored-by: Conall Ó Cofaigh <ocofaigh@ie.ibm.com>

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -44,7 +44,7 @@ data "ibm_is_ssh_key" "existing_ssh_key" {
 
 module "slz_vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.11.0"
+  version           = "7.13.2"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
