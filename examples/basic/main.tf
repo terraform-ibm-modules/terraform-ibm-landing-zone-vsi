bc32fb91da21e8ed1a48d1584f37588ff84ae560
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-07-27 13:13:58 +0000

    chore(deps): update terraform-module

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -45,7 +45,7 @@ data "ibm_is_ssh_key" "existing_ssh_key" {
 
 module "slz_vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.18.3"
+  version           = "7.19.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
