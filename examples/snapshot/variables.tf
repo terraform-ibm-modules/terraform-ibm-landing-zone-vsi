7e1af595646038b81d638bfe0758ba554821359e
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-08-28 09:57:00 +0100

    chore: update the default VSI image IDs (#717)
    Co-authored-by: Aashiq-J <122446118+Aashiq-J@users.noreply.github.com>

--- examples/snapshot/variables.tf
+++ examples/snapshot/variables.tf
@@ -37,7 +37,7 @@ variable "access_tags" {
 variable "image_id" {
   description = "Image ID used for VSI. Run 'ibmcloud is images' to find available images. Be aware that region is important for the image since the id's are different in each region."
   type        = string
-  default     = "r014-2a39e91e-e899-4ac4-b309-57e107a7819f" # NOTE: this ID is for us-east region, Redhat 8.10 minimal
+  default     = "r014-e270d889-f4c6-4d48-8910-026756aaecc4" # NOTE: this ID is for us-east region, Redhat 8.10 minimal
 }
 
 variable "machine_type" {
