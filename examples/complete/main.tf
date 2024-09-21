fa165cf733c9d3dd6ac9a4222a071898ce2cce42
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-09-21 01:35:41 +0100

    chore(deps): update terraform terraform-ibm-modules/kms-all-inclusive/ibm to v4.15.13 (#730)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -24,7 +24,7 @@ module "resource_group" {
 
 module "key_protect_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "4.15.11"
+  version                   = "4.15.13"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
   key_protect_instance_name = "${var.prefix}-kp"
