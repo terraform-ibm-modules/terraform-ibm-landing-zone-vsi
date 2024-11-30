4170f98332fd613ecc29cce21587675572fa3173
Terraform.IBM.Modules.Operations@ibm.com <Terraform.IBM.Modules.Operations@ibm.com>
2024-11-30 11:16:02 +0000

    chore: update the default VSI image IDs

--- examples/fscloud/variables.tf
+++ examples/fscloud/variables.tf
@@ -31,7 +31,7 @@ variable "resource_tags" {
 variable "image_id" {
   description = "Image ID used for VSI. Run 'ibmcloud is images' to find available images in a region"
   type        = string
-  default     = "r006-ec03e14e-6336-4fe6-ba4f-460e266c6b10"
+  default     = "r006-0b7c7439-70c7-4c29-98df-52759b012f63"
 }
 
 variable "machine_type" {
