bfdde32878382dbc210fb8a50a80a340b66d8d3e
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-03-27 11:07:56 +0530

    update test

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -32,3 +32,12 @@ module "vpc" {
 data "ibm_is_image" "image" {
   name = "ibm-ubuntu-24-04-6-minimal-amd64-3"
 }
+
+module "secrets_manager" {
+  source               = "terraform-ibm-modules/secrets-manager/ibm"
+  version              = "1.26.0"
+  resource_group_id    = module.resource_group.resource_group_id
+  region               = var.region
+  secrets_manager_name = "${var.prefix}-sm"
+  sm_tags              = var.resource_tags
+}
