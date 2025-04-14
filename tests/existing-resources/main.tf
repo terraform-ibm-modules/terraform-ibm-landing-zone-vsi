eaa65dac008250f547d4b8d9d117844e25e70759
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-04-14 14:06:21 +0100

    fix(deps): update terraform-module (#825)

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.1.6"
+  version                      = "1.2.0"
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
@@ -11,7 +11,7 @@ module "resource_group" {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.19.1"
+  version           = "7.22.10"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   name              = "vpc"
