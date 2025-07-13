1d940504be5a9f501439de7afda130416e937437
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-13 21:29:56 +0000

    fix(deps): update terraform dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -24,7 +24,7 @@ module "resource_group" {
 
 module "key_protect_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "5.1.9"
+  version                   = "5.1.11"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
   key_protect_instance_name = "${var.prefix}-kp"
@@ -57,7 +57,7 @@ module "key_protect_all_inclusive" {
 
 module "logging" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.5.0"
+  version           = "1.5.5"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   resource_tags     = var.resource_tags
@@ -101,7 +101,7 @@ data "ibm_is_ssh_key" "existing_ssh_key" {
 
 module "slz_vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.25.9"
+  version           = "7.25.10"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
