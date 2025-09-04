2e9ca9c36a07ba4755d5b30c6543c5d69b5ab05f
Rajat Agrawal <108140212+rajatagarwal-ibm@users.noreply.github.com>
2025-09-04 10:27:44 +0100

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -93,7 +93,7 @@ resource "ibm_is_security_group_rule" "security_group_rules" {
       ? [] # if all values null empty array
       : [each.value]
     )
-    # Conditianally add content if sg has icmp
+    # Conditionally add content if sg has icmp
     content {
       type = lookup(
         each.value["icmp"],
