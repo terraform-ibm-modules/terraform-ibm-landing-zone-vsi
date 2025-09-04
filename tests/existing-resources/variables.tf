7fb6346173d739470d7b6e988fb9dde2918b1529
Rajat Agrawal <108140212+rajatagarwal-ibm@users.noreply.github.com>
2025-09-04 10:33:17 +0100

    Update variables.tf

--- tests/existing-resources/variables.tf
+++ tests/existing-resources/variables.tf
@@ -34,5 +34,5 @@ variable "resource_tags" {
 variable "image_name" {
   type        = string
   default     = "ibm-ubuntu-24-04-3-minimal-amd64-1"
-  description = "Image name to retrive the image ID for that region."
+  description = "Image name to retrieve the image ID for that region."
 }
