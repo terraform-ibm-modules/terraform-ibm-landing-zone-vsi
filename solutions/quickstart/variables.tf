a0eddd9e7614fb81f9ec8c124d4b9d99898ff7f1
Akash Kumar <akash.kumar@ibm.com>
2025-09-12 18:34:18 +0530

    testing

--- solutions/quickstart/variables.tf
+++ solutions/quickstart/variables.tf
@@ -8,12 +8,6 @@ variable "ibmcloud_api_key" {
   sensitive   = true
 }
 
-variable "region" {
-  type        = string
-  description = "Region in which all the resources will be deployed. [Learn More](https://terraform-ibm-modules.github.io/documentation/#/region)."
-  default     = "us-south"
-}
-
 variable "existing_resource_group_name" {
   type        = string
   description = "The name of an existing resource group to provision the resources. If not provided the default resource group will be used."
