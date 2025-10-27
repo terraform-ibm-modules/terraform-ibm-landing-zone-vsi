43fbb27ca3780fcf21508263dd7d88297da39b15
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-10-27 14:26:11 +0530

    fix typo

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -1,7 +1,6 @@
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
   version                      = "1.4.0"
-  version                      = "1.4.0"
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
