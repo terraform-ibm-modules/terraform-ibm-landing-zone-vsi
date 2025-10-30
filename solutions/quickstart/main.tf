4a6fe1a285b05ac87e90cc2085efab6f66637c78
Akash Kumar <akash.kumar@ibm.com>
2025-10-30 17:18:27 +0530

    prefix fix

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -9,7 +9,7 @@ module "resource_group" {
 
 locals {
   ssh_key_id = var.existing_ssh_key_name != null ? data.ibm_is_ssh_key.existing_ssh_key[0].id : resource.ibm_is_ssh_key.ssh_key[0].id
-  prefix     = var.prefix != null ? trimspace(var.prefix) != "" ? "${var.prefix}-" : "" : ""
+  prefix     = var.prefix != null ? trimspace(var.prefix) != "" ? "${var.prefix}" : "" : ""
 }
 
 ##############################################################################
@@ -24,7 +24,7 @@ resource "tls_private_key" "tls_key" {
 
 resource "ibm_is_ssh_key" "ssh_key" {
   count      = var.existing_ssh_key_name != null ? 0 : 1
-  name       = "${local.prefix}-ssh-key"
+  name       = "${local.prefix}ssh-key"
   public_key = resource.tls_private_key.tls_key[0].public_key_openssh
 }
 
@@ -55,7 +55,7 @@ module "vpc" {
         no_addr_prefix = false
       }
   ] }
-  name = "${local.prefix}-qs-vpc"
+  name = "${local.prefix}qs-vpc"
   network_acls = [
     {
       name                         = "vpc-acl"
