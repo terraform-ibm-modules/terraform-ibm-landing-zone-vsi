aa3cde67028f83fa37d460d337fbfe5c1dedffd1
Piyush Kumar Sahu <Piyush.Kumar.Sahu@ibm.com>
2025-11-28 11:54:16 +0530

    small fix

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -34,7 +34,7 @@ variable "prefix" {
 variable "provider_visibility" {
   description = "Set the visibility value for the IBM terraform provider. Supported values are `public`, `private`, `public-and-private`. [Learn more](https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/guides/custom-service-endpoints)."
   type        = string
-  default     = "private"
+  default     = "public"
   nullable    = false
 
   validation {
