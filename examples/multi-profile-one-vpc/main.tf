21821a25f3d2eec07dd8eff1f3c5e990b1d6ac42
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-11 06:05:35 +0000

    fix(deps): update dependencies (#1011)

--- examples/multi-profile-one-vpc/main.tf
+++ examples/multi-profile-one-vpc/main.tf
@@ -120,7 +120,7 @@ data "ibm_is_ssh_key" "existing_ssh_key" {
 
 module "slz_vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.10.5"
+  version           = "8.10.6"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
