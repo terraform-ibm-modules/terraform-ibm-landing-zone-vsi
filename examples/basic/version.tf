4a9e4db3e9fcae521af4ce1fca88c52eb94199d5
Piyush Kumar Sahu <Piyush.Kumar.Sahu@ibm.com>
2026-05-07 20:30:11 +0530

    updated versions

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "= 1.88.0"
+      version = "2.1.0"
     }
     tls = {
       source  = "hashicorp/tls"
