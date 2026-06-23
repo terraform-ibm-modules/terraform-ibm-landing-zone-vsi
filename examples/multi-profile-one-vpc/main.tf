b2dfa540293d21b08caa1005908dd1f53ea437db
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-06-23 15:00:47 +0530

    fix(deps): update dependencies (#1064)

--- examples/multi-profile-one-vpc/main.tf
+++ examples/multi-profile-one-vpc/main.tf
@@ -44,7 +44,7 @@ module "key_protect_all_inclusive" {
 
 module "existing_boot_volume_kms_key_crn_parser" {
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.6.1"
+  version = "1.8.0"
   crn     = module.key_protect_all_inclusive.keys["slz-vsi.${var.prefix}-vsi"].crn
 }
 
@@ -120,7 +120,7 @@ data "ibm_is_ssh_key" "existing_ssh_key" {
 
 module "slz_vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "9.0.7"
+  version           = "9.0.8"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
@@ -207,7 +207,7 @@ locals {
 
 module "vsi_image_selector" {
   source           = "terraform-ibm-modules/common-utilities/ibm//modules/vsi-image-selector"
-  version          = "1.6.1"
+  version          = "1.8.0"
   architecture     = "amd64"
   operating_system = "ubuntu"
 }
