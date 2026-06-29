61369e46bde7ec9e0b0b92c9478bc6a4c44901d0
root <root@pipelb73d583f5d21f34827c72842ac201c8795111fb9579e2edd634fcc-pod>
2026-06-29 05:40:25 +0000

    fix(deps): update dependencies

--- solutions/quickstart/variables.tf
+++ solutions/quickstart/variables.tf
@@ -87,7 +87,7 @@ variable "vsi_name" {
 variable "image_name" {
   description = "Image ID used for Virtual server instance. [Learn more](https://cloud.ibm.com/docs/vpc?topic=vpc-about-images)"
   type        = string
-  default     = "ibm-ubuntu-26-04-minimal-amd64-2"
+  default     = "ibm-ubuntu-22-04-5-minimal-amd64-16"
 }
 
 variable "size" {
